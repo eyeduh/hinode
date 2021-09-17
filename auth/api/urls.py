7d039e0d5110e86478d964e7abf75b53f3c80540
Aida Noori <ida@Aidas-MacBook-Pro.local>
2021-09-18 01:50:35 +0430

    deleted unwanted directory

--- auth/api/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from accounts.api.views import user_login_view, user_register_view, obtain_token_view
-
-urlpatterns = [
-    path('login/', user_login_view, name='user_login'),
-    path('login/step-1/', user_register_view, name='user_login_step_1'),
-    path('login/step-2/', obtain_token_view, name='user_login_step_2'),
-]
